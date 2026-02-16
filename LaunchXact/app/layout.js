8aff8c4b58ad7ed2584bf7ca00755a1918b4e822
Ravi Joshi <rj7350215@gmail.com>
2026-02-16 20:04:42 +0530

    Fix: Migrate GTM to @next/third-parties for better compatibility

--- LaunchXact/app/layout.js
+++ LaunchXact/app/layout.js
@@ -3,6 +3,7 @@ import Footer from '../components/Footer';
 import './globals.css';
 import { Inter, Playfair_Display } from 'next/font/google';
 import Script from 'next/script';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 const playfair = Playfair_Display({ subsets: ['latin'], variable: '--font-serif' });
@@ -58,28 +59,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.variable} ${playfair.variable}`}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PJRNX6SW"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
+        {/* Google Tag Manager (Implemented via @next/third-parties) */}
+        <GoogleTagManager gtmId="GTM-PJRNX6SW" />
 
-        {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PJRNX6SW');
-          `}
-        </Script>
-        {/* End Google Tag Manager */}
 
         {/* Google Analytics (Legacy/Direct) */}
         <Script
